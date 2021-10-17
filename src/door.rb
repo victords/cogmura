a55a3dac4ae23d79735829580e59ee4156ce1890
Victor David Santos <victordavidsantos@gmail.com>
2021-10-17 10:07:31 -0300

    Draw door action indicator.

--- src/door.rb
+++ src/door.rb
@@ -19,19 +19,32 @@ class Door < IsoGameObject
     if type != 2
       @sub_img = @img.map { |img| img.subimage(0, 0, img.width / 2, img.height) }
     end
+
+    @alert = Res.img(:alert)
   end
 
   def update(man)
     if @opening
       animate_once([1, 2, 3, 4], 7)
-    elsif man.bounds.intersect?(bounds) && KB.key_pressed?(Gosu::KB_Z)
-      @on_open.call(self)
-      @opening = true
+    elsif man.bounds.intersect?(bounds)
+      @can_open = true
+      if KB.key_pressed?(Gosu::KB_Z)
+        @on_open.call(self)
+        @opening = true
+      end
+    else
+      @can_open = false
     end
   end
 
   def draw(map)
     super(map, @type == 1 ? :horiz : nil, @z_index)
+
+    if @can_open && !@opening
+      @alert.draw(@screen_x + @img_gap.x + @img[0].width / 2 * Graphics::SCALE - 4, @screen_y + @img_gap.y - 28,
+                  @z_index + 1, Graphics::SCALE, Graphics::SCALE)
+    end
+
     return unless @sub_img
 
     x_offset = @type == 0 ? 0 : @img[0].width * Graphics::SCALE
